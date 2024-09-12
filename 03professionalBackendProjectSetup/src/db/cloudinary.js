9ec2c143ce6c8d51c7976f8c89e052efc4eb8f28
theayush434 <theayush434@gmail.com>
2024-09-12 22:03:43 +0530

    cloudinary

---
+++ 03professionalBackendProjectSetup/src/db/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+cloudinary.config({ 
+     cloud_name: 'dvzq79zrn', 
+     api_key: '744837115849658', 
+     api_secret: '<your_api_secret>' 
+ });
